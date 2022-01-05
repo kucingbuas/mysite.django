ed75ad531e3ee5413db4d6eb415f3c2896ee7e52
Rama Jawara <1811102441081@umkt.ac.id>
2022-01-06 00:32:47 +0800

    third commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = 'django-insecure-i9n=(&3d%a^r*h%(5!jz&tw13v++xw*kziy6xv0m$&us@op-vv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*', '192.168.2.1']
-ALLOWED_HOSTS = ['ramasite-app.herokuapp.com']
+ALLOWED_HOSTS = ['*', '192.168.2.1']
+# ALLOWED_HOSTS = ['ramasite-app.herokuapp.com']
 
 
 # Application definition
@@ -161,22 +161,22 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-        'default':{
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'd72ie0qq16pb77',
-            'USER': 'ezkpazvzjhpebw',
-            'PASSWORD': '377d5a203cb1d8f09589cf5ce569c676bdfe0874f128e27ed7463977564e2bb2',
-            'HOST': 'ec2-3-224-251-59.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+# DATABASES = {
+#         'default':{
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'd72ie0qq16pb77',
+#             'USER': 'ezkpazvzjhpebw',
+#             'PASSWORD': '377d5a203cb1d8f09589cf5ce569c676bdfe0874f128e27ed7463977564e2bb2',
+#             'HOST': 'ec2-3-224-251-59.compute-1.amazonaws.com',
+#             'PORT': '5432',
+#         }
+#     }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
